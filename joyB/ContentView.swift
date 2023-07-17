a7debd1ba1f8a9d564b81f2d65d4992234d6ad36
divya <divyaaddepalle@gmail.com>
2023-07-17 09:54:35 -0500

    Initial Commit

---
+++ joyB/ContentView.swift
@@ -0,0 +1,57 @@
+//
+//  ContentView.swift
+//  joyB
+//
+//  Created by Scholar on 7/13/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+@State private var aboutMe = false
+
+    var body: some View {
+        
+        ZStack {
+            Color(.systemGray4)
+                .ignoresSafeArea()
+            
+        VStack(alignment: .leading, spacing: 20.0) {
+            
+                Image("me")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .cornerRadius(15)
+            Button (action: {aboutMe.toggle()}){
+                Text ("click me to see some facts about me!")
+                    .multilineTextAlignment(.center)
+                  
+            }
+            if aboutMe {
+                Text ("I love reading, mangos, sushi, listening to music and going on walks!")
+            }
+                
+                
+                   
+                }
+               
+            }
+        //    .padding()
+      //      .cornerRadius(15)
+            .shadow(radius: 15)
+//            .padding()
+            .buttonStyle(.borderedProminent)
+            .tint(.black)
+        }
+        
+       
+        
+    }
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ joyB/joyBApp.swift
@@ -0,0 +1,17 @@
+//
+//  joyBApp.swift
+//  joyB
+//
+//  Created by Scholar on 7/13/23.
+//
+
+import SwiftUI
+
+@main
+struct joyBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
